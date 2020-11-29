94e4aa3a71bd883705f3f4172d84fec48091d62e
miriSilverman <mirisboom@gmail.com>
2020-11-29 22:29:58 +0200

    init

---
+++ Assets/scripts/BackGroundMusic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class BackGroundMusic : MonoBehaviour
+{
+   private static BackGroundMusic bgmInstance;
+   private void Awake()
+   {
+      DontDestroyOnLoad(transform.gameObject);
+
+      // onlu one instance of background sound
+      if (bgmInstance == null)
+      {
+         bgmInstance = this;
+      }
+      else
+      {
+         Destroy(gameObject);
+      }
+   }
+}
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UI;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
